635b2c71db6bd2e5a56b0074ebb478fef1d98b7c
ZackWestern <zhammond@uwo.ca>
2024-11-22 20:10:13 -0500

    e

--- Assets/Scripts/RecoilPattern.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewRecoilPattern", menuName = "Recoil/RecoilPattern")]
-public class RecoilPattern : ScriptableObject
-{
-    public float[] horizontalRecoil;
-    public float[] verticalRecoil;
-}
